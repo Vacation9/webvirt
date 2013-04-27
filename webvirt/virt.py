04117f5a6f3dc921b5172fc7d36fc7f80769a37e
James Forcier <2014jforcier@tjhsst.edu>
2013-04-27 04:08:05 -0400

    fix jacob's problems (if only it were this simple)

--- webvirt/virt.py
+++ webvirt/virt.py
@@ -69,7 +69,7 @@ class HostServer:
                     </devices>
                 </domain>
                 """ % (name,mem,cpu,hd,iso,pts)
-        )
+         )
          self.domains.append(dom)
                 return dom
 

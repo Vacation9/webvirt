32f7afb47dd9400b1d077e3a0d6b2a87299beec2
James Forcier <2014jforcier@tjhsst.edu>
2013-04-27 01:09:26 -0400

    console yet again

--- webvirt/urls.py
+++ webvirt/urls.py
@@ -69,12 +69,12 @@ class Console:
         templates = web.template.render('webvirt/templates/')
 	domObj = conn.lookupByName(web.input()['domain'])
 	streamObj = libvirt.virStream(conn)
-	'''streamObjStatus = domObj.openConsole("/dev/pts/15", streamObj, 0)
+	streamObjStatus = domObj.openConsole("/dev/pts/15", streamObj, 0)
 	if streamObjStatus == 0:
 	    return streamObj
 	elif streamObjStatus == -1:
 	    return 'Error opening stream object'
 	else:
-	    return 'Something very, very bad happened'''
+	    return 'Something very, very bad happened'
 
 classes = globals()

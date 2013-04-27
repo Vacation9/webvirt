a3b8b13e8b74a476f7c7e5d8e2c1830cb5482dce
Peter Foley <pefoley2@verizon.net>
2013-04-27 01:18:22 -0400

    host data

--- webvirt/virt.py
+++ webvirt/virt.py
@@ -7,3 +7,14 @@ class Domain:
         self.name = self.dom.name()
         self.state = common.getState(self.dom.state(0)[0])
 
+class Host:
+    def __init__(self):
+        self.hostname = conn.getHostname()
+        self.type = conn.getType()
+        self.caps = conn.getCapabilites()
+        self.cpustats = conn.getCPUStats(libvirt.VIR_NODE_CPU_STATS_ALL_CPUS,0)
+        self.cpumap = conn.getCPUMap(0)
+        self.info = conn.getInfo()
+        self.memstats = conn.getMemoryStats(libvirt.VIR_NODE_MEMORY_STATS_ALL_CELLS,0)
+    def createDomain(self):
+        return 'bob'

24a4c19893937ccee6cb2a3b576bb94809cba882
Michael Smith <michael@s0lder.com>
2013-04-27 04:18:48 -0400

    remove ajax files

--- static/js/ajax.js
+++
@@ -1,7 +0,0 @@
-function makeRequest(url, callback_builder, method) {
-    var request = new XMLHttpRequest();
-    callback = callback_builder(request);
-    request.onreadystatechange = callback;
-    request.open(method, url, true);
-    request.send(null);
-}
